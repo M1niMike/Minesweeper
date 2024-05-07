ba1b44a51159cab542e385b21ab28f45407289ec
Micael Eid <Mikaeleid@hotmail.com>
2024-05-07 21:22:46 +0100

    Criação dos modulos + main-menu.component

---
+++ minesweeper/src/components/main-menu/main-menu.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./main-menu.css";
+
+function MainMenu() {
+  return (
+    <div className="panel">
+      <h1>Aqui</h1>
+    </div>
+  );
+}
+
+export default MainMenu;
